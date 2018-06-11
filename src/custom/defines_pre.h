5cdd68645b54067da66f2020214446dc2000961b
rons003 <ronelle003@gmail.com>
2018-06-12 04:28:11 +0800

    Uncomment Packetver

--- src/custom/defines_pre.h
+++ src/custom/defines_pre.h
@@ -7,6 +7,6 @@
  * rAthena configuration file (http://rathena.org)
  * For detailed guidance on these check http://rathena.org/wiki/SRC/config/
  **/
-//#define PACKETVER 20170614
+#define PACKETVER 20170614
 
 #endif // _CONFIG_CUSTOM_DEFINES_PRE_H_
